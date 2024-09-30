36482d5dd3716ba0f45a3768d4755b2d8c5e5b31
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2024-09-30 19:27:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -364,10 +364,6 @@ resource "aws_s3_bucket" "example" {
   bucket = "pipleinbucket0088bdhuwtrrr"
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
-}
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.example.id
