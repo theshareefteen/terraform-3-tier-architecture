5352f5dc71af4324081e843d933a77f08e652012
shoeb-dev <shoxibahmed077@gmail.com>
2024-10-04 12:11:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket = "terrastatebyucket0078970778"
+    bucket = "terrastatebyucketforproject6"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
